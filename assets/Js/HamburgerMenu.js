295ee8647e1ec3f5af3367c58d29c57013f28d2c
ShenanVindinu <shenanvindinu@gmail.com>
2024-06-13 21:53:17 +0530

    extra comma removed

--- assets/Js/HamburgerMenu.js
+++ assets/Js/HamburgerMenu.js
@@ -1,7 +1,5 @@
 $("#hamburgerButton").eq(0).on('click', () => {
-    $("#hamPage").css({
-        display: 'block',
-    });
+    $("#hamPage").css({display: 'block'});
 });
 
 $("#hamburgerButtonClose").eq(0).on('click', () => {
